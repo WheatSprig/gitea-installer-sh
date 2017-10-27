6a34cb6eaaf01aba301b3828bcc8b89f96e2bc57
AJ ONeal <aj@daplie.com>
2017-10-27 17:57:22 -0600

    add install script

---
+++ install.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# wget -O - https://git.coolaj86.com/coolaj86/gitea-installer/raw/master/install.bash | bash
+
+# Create a 'gitea' user and group with the home /opt/gitea
+sudo adduser gitea --home /opt/gitea
+
+# Make some other potentially useful directories for that user/group
+sudo mkdir -p /opt/gitea/ /var/log/gitea
+sudo chown -R gitea:gitea /opt/gitea/ /var/log/gitea
+
+# Download and install gitea
+sudo wget -O /opt/gitea/gitea https://dl.gitea.io/gitea/1.0.1/gitea-1.0.1-linux-amd64
+sudo chmod +x gitea
+
+# Download and install the gitea.service for systemd
+sudo wget -O /etc/systemd/system/gitea.service https://git.coolaj86.com/coolaj86/gitea-installer/raw/master/dist/etc/systemd/system/gitea.service
+
+# Start gitea
+sudo systemctl restart gitea
