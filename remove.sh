90d27beaf606f5f8c262d7b9917b9602fcd9289b
Josh Mudge <josh@cityofardon.com>
2018-07-26 15:20:54 -0600

    Make removal script more verbose.

--- remove.sh
+++ remove.sh
@@ -3,10 +3,17 @@
 
 echo "Removing Gitea and DELETING ALL DATA."
 
+echo "Stopping and removing the Gitea service."
 sudo systemctl stop gitea # Stop the Gitea service
 sudo systemctl disable gitea # Disable the Gitea service automatically starting on boot.
 sudo rm /etc/systemd/system/gitea.service # Delete the Gitea service.
+
+echo "Deleting Gitea configuration and Data."
+
 sudo rm /usr/local/bin/gitea # Remove Gitea from Path
 sudo rm -rf /opt/gitea # Remove Gitea Data
 
-echo "Done."
+if test ! -d /opt/gitea
+   echo "Gitea has been completely removed."
+else
+  echo "Gitea has not been completely removed. File an issue here: https://git.coolaj86.com/coolaj86/gitea-installer.sh/issues/new"
